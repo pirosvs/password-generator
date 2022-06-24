af0a89e487e8643d5f7505c8873a9e2ed04f6eeb
darmstrong <armstrong2579@gmail.com>
2022-06-24 00:58:18 -0700

    add comments for next steps etc

--- script.js
+++ script.js
@@ -11,11 +11,23 @@ function writePassword() {
 
 }
 
+// KNOWN ISSUES:
+// - NOTHING RUNS AFTER PROMPT WINDOW
+// - NEED TO CHECK THAT CONFIRMS ARE ADDING CORRECT VALUE TO ARRAY OF FUNCTIONS, ETC
+
 var charLowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var charUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var charNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 // do they need to be strings or can they just be ints?
 var charSymb = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
+var selectedChoice = [];
+// needs to become randomChoice = [randomLowCase, randomeUpperCase, randomNum, randomSymb]; dependent on which
+// option chosen
+
+// CURRENTLY:
+// - PER CONFIRM THAT RETURNS TRUE, ADDS THAT TRUE TO VARIABLE SELECTED CHOICE
+// - SELECTED CHOICE DOES NOT CONTAIN THE RANDOMIZED FUNCTIONS FOR EACH CHAR SET
+// --> ASK ABOUT HOW TO LINK THE TRUE VALUES FROM CONFIRMS TO RANDOM CHAR GEN TO ARRAY FOR EACH RANDOM GEN
 
 function generatePassword()
 {
@@ -23,6 +35,7 @@ function generatePassword()
   if (yesGen) {
     var passLength = window.prompt("How many characters would you like to include (must be between 8 and 128)?", [num]);
     console.log(passLength);
+    console.log(charLowCase[0]);
     // var not taking on value of user input? -- won't print in console
     // runs prompt and accepts input but refuses to run anything after that even console.log?
     console.log(typeof passLength);
@@ -30,44 +43,48 @@ function generatePassword()
       // problem begins here -- does not pop up confirms once a number is entered
       var lowCase = window.confirm("Would you like to include lower case letters?");
       // do i need to have the following conditionals if using a confirm?
-      // if (lowCase) {
-      //   lowCase = true;
+      if (lowCase) {
+        selectedChoice.push(lowCase);
       // } else {
       //   lowCase = false;
-      // }
+      }
 
       var upperCase = window.confirm("Would you like to include upper case letters?");
-      // if (upperCase) {
-      //   upperCase = true;
+      if (upperCase) {
+        selectedChoice.push(upperCase);
       // } else {
       //   upperCase = false;
-      // }
+      }
 
       var useNum = window.confirm("Would you like to include numbers?");
-      // if (useNum) {
-      //   useNum = true;
+      if (useNum) {
+        selectedChoice.push(useNum);
       // } else {
       //   useNum = false;
-      // }
+      }
 
       var useSymb = window.confirm("Would you like to include special characters?");
-      // if (useSymb) {
-      //   useSymb = true;
+      if (useSymb) {
+        selectedChoice.push(useSymb);
       // } else {
       //   useSymb = false;
-      // }
+      }
+
+      // do i need to create a variable to contain which options selected? can use conditional w/in
+      // each confirm to (add? return true?) push to add each to array randomChoice if return true?
 
       if (lowCase == false && upperCase == false && useNum == false && useSymb == false) {
         alert("Password must contain at least one character choice.");
       } else {
         // do i need an else if for every possible set of chars chosen?
         for (i=0; i < passLength.length; i++) {
-
+          // how do I randomize which thing it chooses of the selected options?
+          randomChoice = selectedChoice[Math.floor(Math.random() * selectedChoice)];
         }
       }
 
     } else {
-      alert("Password must be between 8 and 128 characters!");
+      alert("Password must be between 8 and 128 characters.");
     }
   }
 
@@ -78,10 +95,32 @@ function generatePassword()
 generateBtn.addEventListener("click", writePassword);
 
 
-// randomLowCase = useLowCase[Math.floor(Math.random() * useLowCase.length)]
-// randomUpperCase = useUpperCase[Math.floor(Math.random() * useUpperCase.length)]
-// randomNum = useNum[Math.floor(Math.random() * useNum.length)]
-// randomSymb = useSymb[Math.floor(Math.random() * useSymb.length)]
+// would it look like:
+// var randonLowCase = function(charLowCase[Math.floor(Math.random() * charLowCase.length)]);
+
+// function randomLowCase() {
+  // return charLowCase[Math.floor(Math.random() * charLowCase.length)];}
+
+// randomLowCase = charLowCase[Math.floor(Math.random() * charLowCase.length)];
+
+// ---------------------------------------------------------------------
+
+// function () {
+  // return randomChoice = randomChoice[Math.floor(Math.random() * randomChoice)];}
+  // may need to change name before =, meant to randomize which char set to choose
+
+// function () {
+  // return randomLowCase = charLowCase[Math.floor(Math.random() * charLowCase.length)];}
+
+// function () {
+//  return randomUpperCase = charUpperCase[Math.floor(Math.random() * charUpperCase.length)];}
+
+// function () {
+//  return randomNum = charNum[Math.floor(Math.random() * charNum.length)];}
+
+// function () {
+//  return randomSymb = charSymb[Math.floor(Math.random() * charSymb.length)];}
+
 // duch512 @ https://gist.github.com/bendc/1e6af8f2d8027f2965da
 
   // all your code should go in here
