b78af835359201e338bb8a528af5c058eec7d2d0
darmstrong <armstrong2579@gmail.com>
2022-06-23 23:50:04 -0700

    add comments for attempts to debug prompt

--- script.js
+++ script.js
@@ -21,36 +21,41 @@ function generatePassword()
 {
   var yesGen = window.confirm("Would you like to generate a password?");
   if (yesGen) {
-    var passLength = window.prompt("How many characters would you like to include (must be between 8 and 128)?");
-    if (passLength >= 8 || passLength <= 128) {
+    var passLength = window.prompt("How many characters would you like to include (must be between 8 and 128)?", [num]);
+    console.log(passLength);
+    // var not taking on value of user input? -- won't print in console
+    // runs prompt and accepts input but refuses to run anything after that even console.log?
+    console.log(typeof passLength);
+    if (passLength >== 8 && passLength <== 128) {
       // problem begins here -- does not pop up confirms once a number is entered
       var lowCase = window.confirm("Would you like to include lower case letters?");
-      if (lowCase) {
-        lowCase = true;
-      } else {
-        lowCase = false;
-      }
+      // do i need to have the following conditionals if using a confirm?
+      // if (lowCase) {
+      //   lowCase = true;
+      // } else {
+      //   lowCase = false;
+      // }
 
       var upperCase = window.confirm("Would you like to include upper case letters?");
-      if (upperCase) {
-        upperCase = true;
-      } else {
-        upperCase = false;
-      }
+      // if (upperCase) {
+      //   upperCase = true;
+      // } else {
+      //   upperCase = false;
+      // }
 
       var useNum = window.confirm("Would you like to include numbers?");
-      if (useNum) {
-        useNum = true;
-      } else {
-        useNum = false;
-      }
+      // if (useNum) {
+      //   useNum = true;
+      // } else {
+      //   useNum = false;
+      // }
 
       var useSymb = window.confirm("Would you like to include special characters?");
-      if (useSymb) {
-        useSymb = true;
-      } else {
-        useSymb = false;
-      }
+      // if (useSymb) {
+      //   useSymb = true;
+      // } else {
+      //   useSymb = false;
+      // }
 
       if (lowCase == false && upperCase == false && useNum == false && useSymb == false) {
         alert("Password must contain at least one character choice.");
@@ -66,17 +71,6 @@ function generatePassword()
     }
   }
 
-  // all your code should go in here
-  // prompt the user or whatever you're supposed to do
-
-  // needsNumber = create a variable to store if the user wants a number in their password
-  // ask the user if they need a number in the password
-  // if they do, 
-  //    then set our other variable to true
-  // ask the user if they need a symbol in the password
-  // if needsNumber
-  //  then generate a password with a number
-
   return "placeholder password";
 }
 
@@ -89,3 +83,14 @@ generateBtn.addEventListener("click", writePassword);
 // randomNum = useNum[Math.floor(Math.random() * useNum.length)]
 // randomSymb = useSymb[Math.floor(Math.random() * useSymb.length)]
 // duch512 @ https://gist.github.com/bendc/1e6af8f2d8027f2965da
+
+  // all your code should go in here
+  // prompt the user or whatever you're supposed to do
+
+  // needsNumber = create a variable to store if the user wants a number in their password
+  // ask the user if they need a number in the password
+  // if they do, 
+  //    then set our other variable to true
+  // ask the user if they need a symbol in the password
+  // if needsNumber
+  //  then generate a password with a number
