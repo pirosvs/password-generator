63f40acf1ce2cfd825493946bd07df08742b5d61
darmstrong <armstrong2579@gmail.com>
2022-06-24 18:47:59 -0700

    change push to concat, function seems to work but runs once & returns nonsense value

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ function writePassword() {
 }
 
 // KNOWN ISSUES:
-// - NEED TO CHECK THAT CONFIRMS ARE ADDING CORRECT VALUE TO ARRAY OF FUNCTIONS, ETC
+// - IF GENERATE A SECOND PASSWORD WITHOUT REFRESHING, DOUBLES -- i need to clear it afterwards a;sdkfj;skldjf
+// - DOES NOT GIVE ALERT IF NUMBER TOO HIGH, ONLY TOO LOW
 
 var charLowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var charUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -20,6 +21,23 @@ var charNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 // do they need to be strings or can they just be ints?
 var charSymb = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
 var selectedChoice = [];
+
+// Functions to randomize which character is chosen from a each character set array
+// function randomLowCase() {
+//   return charLowCase = charLowCase[Math.floor(Math.random() * charLowCase.length)];}
+
+// function randomUpperCase() {
+//  return charUpperCase = charUpperCase[Math.floor(Math.random() * charUpperCase.length)];}
+
+// function randomNum() {
+//  return charNum = charNum[Math.floor(Math.random() * charNum.length)];}
+
+// function randomSymb() {
+//  return charSymb = charSymb[Math.floor(Math.random() * charSymb.length)];}
+
+ function randomChoice() {
+  return selectedChoice = selectedChoice[Math.floor(Math.random() * selectedChoice.length)];}
+
 // needs to become randomChoice = [randomLowCase, randomeUpperCase, randomNum, randomSymb]; dependent on which
 // option chosen
 
@@ -33,26 +51,29 @@ function generatePassword()
   var yesGen = window.confirm("Would you like to generate a password?");
   if (yesGen) {
     var passLength = window.prompt("How many characters would you like to include (must be between 8 and 128)?");
+    passLength = parseInt("1000", 10);
+    
     if (passLength >= 8 && passLength <= 128) {
+      console.log(passLength);
+      console.log(typeof passLength);
       var lowCase = window.confirm("Would you like to include lower case letters?");
-      // do i need to have the following conditionals if using a confirm? -- NO
       if (lowCase) {
-        selectedChoice.push(charLowCase);
-      // }
+        selectedChoice = selectedChoice.concat(charLowCase);
+      }
 
       var upperCase = window.confirm("Would you like to include upper case letters?");
       if (upperCase) {
-        selectedChoice.push(charUpperCase);
+        selectedChoice = selectedChoice.concat(charUpperCase);
       }
 
       var useNum = window.confirm("Would you like to include numbers?");
       if (useNum) {
-        selectedChoice.push(charNum);
+        selectedChoice = selectedChoice.concat(charNum);
       }
 
       var useSymb = window.confirm("Would you like to include special characters?");
       if (useSymb) {
-        selectedChoice.push(charSymb);
+        selectedChoice = selectedChoice.concat(charSymb);
       }
 
       console.log(selectedChoice);
@@ -64,18 +85,18 @@ function generatePassword()
         alert("Password must contain at least one character choice.");
       // } else {
       //   // do i need an else if for every possible set of chars chosen?
-      //   for (i=0; i < passLength.length; i++) {
-      //     // how do I randomize which thing it chooses of the selected options?
-      //     randomChoice = selectedChoice[Math.floor(Math.random() * selectedChoice)];
+      // w/e the hell is going on here it is NOT working c:
+      } else if (selectedChoice == charLowCase, charUpperCase, charNum, charSymb) {
+          for (i=0; i < passLength.length; i++) {
+           password = password + randomChoice();
+          } console.log(password);
         }
       }
-
     } else {
       alert("Password must be between 8 and 128 characters.");
     }
-  }
 
-  return "placeholder password";
+  return password;
 }
 
 // Add event listener to generate button
@@ -92,21 +113,7 @@ generateBtn.addEventListener("click", writePassword);
 
 // ---------------------------------------------------------------------
 
-// function () {
-  // return randomChoice = randomChoice[Math.floor(Math.random() * randomChoice)];}
   // may need to change name before =, meant to randomize which char set to choose
-
-// function () {
-  // return randomLowCase = charLowCase[Math.floor(Math.random() * charLowCase.length)];}
-
-// function () {
-//  return randomUpperCase = charUpperCase[Math.floor(Math.random() * charUpperCase.length)];}
-
-// function () {
-//  return randomNum = charNum[Math.floor(Math.random() * charNum.length)];}
-
-// function () {
-//  return randomSymb = charSymb[Math.floor(Math.random() * charSymb.length)];}
 
 // duch512 @ https://gist.github.com/bendc/1e6af8f2d8027f2965da
 
